2216f59df405be95589d5d09f9abd015536ad7f8
Yassine AITALLA <yaitalla@e2r4p10.42.fr>
2017-03-03 14:39:13 +0100

    activation du compte

--- verify_email.php
+++ verify_email.php
@@ -2,22 +2,25 @@
 require 'global.php';
 
 
-// echo "user ok";
-// var_dump($_GET);
-
 if (isset($_GET['email']) && !empty($_GET['email']) AND
 	isset($_GET['hash']) && !empty($_GET['hash']))
 {
 	$email = ($_GET['email']);
 	$hash = ($_GET['hash']);
-	$result = $dbh->prepare("SELECT * FROM users WHERE email = ? AND hash = ?");
+	$result = $dbh->prepare("SELECT * FROM users WHERE email = ? AND hash = ?"); //" AND active = ?");
 	$result->execute(array($email, $hash));
 	$result = $result->fetchAll();
 
-	if (!empty($result))
+	if (!empty($result)) //Activer le compte - active = 1; 
 	{
-		//var_dump($result);
-		echo "user ok";
+		//$dbh->prepare("UPDATE users SET active = '1' WHERE email='".$email."'");
+
+		$dbh->query("UPDATE users SET active = '1' WHERE email='".$email."'");
+		//$dbh->execute();
+		//$dbh->prepare("UPDATE users SET active = 1 WHERE email = ?");
+		//$dbh->execute(array($email));
+		//var_dump($_GET);
+		//echo "user OK";
 	}
 	if (empty($result))
 		echo "no user";
